52b0ed8c832cb856c26b9bd5bb36d301326fc032
Dahrio-Francois <dahrio.francois@mths.ca>
2022-03-21 18:23:34 +0000

    added float.py

---
+++ float.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Dahrio Francois
+# Created on: March 2022
+# This program is designed to add two decimal numbers
+
+
+def main():
+    # this program adds the two decimal numbers
+
+    float = 0.1 + 0.2
+    format_float = "{:.1f}".format(float)
+    print("0.1 + 0.2 = ")
+    print(format_float)
+
+
+if __name__ == "__main__":
+    main()
